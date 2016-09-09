38c526f7778a558e6d71ae1380113a32595ff633
wrobell <wrobell@pld-linux.org>
2016-09-09 20:10:32 +0100

    - implemented push and pull example

---
+++ examples/ex-push.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# aionn - asyncio messaging library based on nanomsg and nnpy
+#
+# Copyright (C) 2016 by Artur Wroblewski <wrobell@riseup.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import asyncio
+import logging
+import aionn
+import nnpy
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+async def writer(pub, delay=1):
+    i = 0
+    while True:
+        await asyncio.sleep(delay)
+        print('sending...')
+        value = '{:04} '.format(i) + 'x' * 100
+        await pub.send(value)
+        print('sent', value[:10])
+        i += 1
+
+socket = aionn.Socket(nnpy.AF_SP, nnpy.PUSH)
+socket.connect('tcp://localhost:5555')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(writer(socket))
+
+# vim: sw=4:et:ai
